720229e0fa2f4f0e8a5aa21f87d6fb2c680771c6
DominikGraczek <114923767+DominikGraczek@users.noreply.github.com>
2024-12-16 02:02:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'; // Use import instead of require
+
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class",
-    theme: false,
-    plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: false,
+  plugins: [daisyui], // Use the imported plugin here
 };
